00f609ce2ac77d3830e44f16200e8798ee8c9100
Tsugami <yslan3000@gmail.com>
2019-08-31 21:39:34 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import discord
+from os import environ
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run(environ['DISCORD_TOKEN'])
